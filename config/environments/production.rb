7036baa690cf70ac636fef016496e671fccdc477
[Luke Banahan] <[lukebanahan@gmail.com]>
2023-12-16 15:21:53 +0000

    updating environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "ec2-54-175-214-83.compute-1.amazonaws.com"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
