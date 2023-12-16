6a7c419aa2acab1e549cee4900300d110a904730
[Luke Banahan] <[lukebanahan@gmail.com]>
2023-12-16 15:34:50 +0000

    config changes

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module PasswordManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.hosts = nil
+
 
     # Configuration for the application, engines, and railties goes here.
     #
